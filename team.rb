bab0db028470a89a9e6130a37c06c93421263f8a
hanako.yamada <h.yamada20210929@gmail.com>
2021-10-12 00:17:32 +0000

    resubmit team.rb

--- team.rb
+++ team.rb
@@ -2,11 +2,11 @@ class Team
 
   attr_accessor :name, :win, :lose, :draw
 
-  def initialize
-    self.name = 'Giants'
-    self.win = 67
-    self.lose = 45
-    self.draw = 8
+  def initialize(name,win,lose,draw)
+    self.name = name
+    self.win = win
+    self.lose = lose
+    self.draw = draw
   end
 
   def show_team_result
@@ -15,37 +15,17 @@ class Team
   end
 end
 
-Giants = Team.new
+Giants = Team.new('Giants',67,45,8)
 
-Tigers = Team.new
-Tigers.name = 'Tigers'
-Tigers.win = 60
-Tigers.lose = 53
-Tigers.draw = 7
+Tigers = Team.new('Tigers',60,53,7)
 
-Dragons = Team.new
-Dragons.name = 'Dragons'
-Dragons.win = 60
-Dragons.lose = 55
-Dragons.draw = 5
+Dragons = Team.new('Dragons',60,55,5)
 
-BayStars = Team.new
-BayStars.name = 'BayStars'
-BayStars.win = 56
-BayStars.lose = 58
-BayStars.draw = 6
+BayStars = Team.new('BayStars',56,58,6)
 
-Carp = Team.new
-Carp.name = 'Carp'
-Carp.win = 52
-Carp.lose = 56
-Carp.draw = 12
+Carp = Team.new('Carp',52,56,12)
 
-Swallows = Team.new
-Swallows.name = 'Swallows'
-Swallows.win = 41
-Swallows.lose = 69
-Swallows.draw = 10
+Swallows = Team.new('Swallows',41,69,10)
 
 Giants.show_team_result
 Tigers.show_team_result
